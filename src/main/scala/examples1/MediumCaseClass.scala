0b15f540b0f27afd7c324a99a87d562a02b51eff
CALLE Ramiro <ramiro.sarmientocalle@mnt.fr>
2021-04-15 16:17:44 +0200

    Inital commit

---
+++ src/main/scala/examples1/MediumCaseClass.scala
@@ -0,0 +1,26 @@
+package examples1
+
+case class MediumCaseClass(
+  param1: Int = 0,
+  param2: Int = 0,
+  param3: Int = 0,
+  param4: Int = 0,
+  param5: Int = 0,
+  param6: Int = 0,
+  param7: Int = 0,
+  param8: Int = 0,
+  param9: Int = 0,
+  param10: Int = 0,
+  param11: Int = 0,
+  param12: Int = 0,
+  param13: Int = 0,
+  param14: Int = 0,
+  param15: Int = 0,
+  param16: Int = 0,
+  param17: Int = 0,
+  param18: Int = 0,
+  param19: Int = 0,
+  param20: Int = 0,
+  param21: Int = 0,
+  param22: Int = 0
+)
